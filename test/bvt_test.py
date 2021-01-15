777bd17f66ee11c23a238d8bf949aa6d8969ef6c
chihao <13552574126@163.com>
2021-01-15 17:26:52 +0800

    Initial commit

---
+++ test/bvt_test.py
@@ -0,0 +1,61 @@
+import os
+
+import yaml
+from appium import webdriver
+import unittest
+import time
+from pytest_testconfig import config
+
+timeout = 30
+poll = 2
+
+
+class IAppBVT(unittest.TestCase):
+
+    def get_data(self):
+        dirname = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+        with open(dirname+'/iAppBVT_Python.yml') as f:
+            data = yaml.safe_load(f)
+        return data
+    def setUp(self):
+        data = self.get_data()
+        desired_caps = {}
+        appium_server_url = data['appium_server_url']['url']
+        desired_caps['platformName'] = data['desired_caps']['platformName']
+        desired_caps['udid'] = data['desired_caps']['udid']
+        desired_caps['deviceName'] = data['desired_caps']['deviceName']
+        desired_caps['appPackage'] = data['desired_caps']['appPackage']
+        desired_caps['appActivity'] = data['desired_caps']['appActivity']
+        desired_caps['automationName'] = data['desired_caps']['automationName']
+        desired_caps['noReset'] = data['desired_caps']['noReset']
+
+        self.driver = webdriver.Remote(appium_server_url, desired_caps)
+
+    def tearDown(self):
+        self.driver.quit()
+
+    def test_bvt(self):
+        print('BVT test is started!')
+        status = self.is_record_event_btn_exist()
+        print(f'The record event button is exist - {status}')
+        self.assertTrue(status, f'Check record_button result is {status}')
+        print('Test finished!')
+
+    def is_record_event_btn_exist(self):
+        elem = self._find_elem_by_xpath('//android.widget.Button[contains(@resource-id,"id/trackEventButton")]')
+        return elem is not None
+
+    def _find_elem_by_xpath(self, elem_xpath, time_out=timeout, raise_exception=True):
+        start = time.time()
+        elem = None
+        while time.time() - start < time_out and elem is None:
+            time.sleep(poll)
+            try:
+                elem = self.driver.find_element_by_xpath(elem_xpath)
+            except Exception:
+                print('by pass the element not found')
+
+        if elem is None and raise_exception:
+            raise LookupError(f'The element which xpath is {elem_xpath} could not be found')
+
+        return elem
